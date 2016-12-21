247edb77dc47cdda7a4dbcd4324c28006ed2af96
Maxim Salnikov <webmaxru@users.noreply.github.com>
2016-12-21 11:41:09 +0100

    Server updated

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+'use strict';
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
 });
